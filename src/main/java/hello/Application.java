262e2f60886bafd9aa1ffd61d19743397e5625b5
vignan2020 <88151516+vickydevo@users.noreply.github.com>
2025-12-17 17:28:10 +0530

    Enable @Timed annotation support for Prometheus
    Added a bean for TimedAspect to support Prometheus metrics.

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,10 +1,12 @@
 package hello;
 
 import java.util.Arrays;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import io.micrometer.core.aop.TimedAspect;
+import io.micrometer.core.instrument.MeterRegistry;
 
 @SpringBootApplication
 public class Application {
@@ -21,4 +23,11 @@ public class Application {
         }
     }
 
+    /**
+     * Enables the @Timed annotation support for Prometheus metrics.
+     */
+    @Bean
+    public TimedAspect timedAspect(MeterRegistry registry) {
+        return new TimedAspect(registry);
+    }
 }
